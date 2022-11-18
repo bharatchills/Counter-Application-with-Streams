e28886ad01503f510bfe366d77c0d6778ce82029
Bharat Soni <bharatchills@gmail.com>
2022-11-18 09:40:19 +0530

    initial commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+class CounterBloc {
+  final _stateStreamController = StreamController<int>();
+  final _eventStreamController = StreamController();
+  late int _counter;
+
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  Stream<int> get counterStream => _stateStreamController.stream;
+
+  StreamSink get eventSink => _eventStreamController.sink;
+  Stream get eventStream => _eventStreamController.stream;
+
+  CounterBloc() {
+    _counter = 0;
+    // Continuing listening events
+    eventStream.listen((event) {
+      if (event == "increment") {
+        _counter++;
+      }
+      if (event == "decrement") {
+        _counter--;
+      }
+      // sink counter variable to Stream
+      counterSink.add(_counter);
+    });
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:counter_stream/counter_bloc.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final counterBloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder(
+                stream: counterBloc.counterStream,
+                initialData: 0,
+                builder: (context, snapshot) {
+                  return Text(
+                    '${snapshot.data}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              //Sink increment event in stream
+              counterBloc.eventSink.add("increment");
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              //Sink decrement event in stream
+              counterBloc.eventSink.add("decrement");
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
